c71650cffc5d02a7c4b8f8e0c7a6f0adb6b3cd55
devls@yahoo.com <devls@yahoo.com>
2021-03-20 18:54:53 +0100

    import gsap + anim header-video

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+alert('hello')
+
 import './styles/main.scss'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.container-miniature-gauche', {
+    scrollTrigger : {
+        trigger : '#main',
+        start : 'top top',
+        scrub : 3,
+        pin : '#main'
+    },
+    x :'-100%'
+})
+gsap.to('.container-miniature-droit', {
+    scrollTrigger : {
+        trigger : '#main',
+        start : 'top top',
+        scrub : 3,
+        pin : '#main'
+    },
+    x :'100%'
+})
