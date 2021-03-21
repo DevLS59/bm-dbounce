d37061f0097a9763a40cb386c032aa622995e300
devls@yahoo.com <devls@yahoo.com>
2021-03-21 14:18:20 +0100

    ajout page video

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/main.scss'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import barba from '@barba/core';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -22,3 +23,50 @@ gsap.to('.container-miniature-droit', {
     },
     x :'100%'
 })
+
+
+
+
+
+
+
+// barba animation 
+// const wipe = document.querySelector('.overlay');
+
+// // const TLAnim = new TimelineMax();
+
+// function delay(n) {
+//   return new Promise((done) => {
+//     setTimeout(() => {
+//       done();
+//     }, n)
+//   })
+// }
+
+// barba.init({
+
+//   sync: true,
+
+//   transitions: [
+//     {
+//       async leave(){
+
+//         const done = this.async();
+
+//         gsap.to(wipe, {left: '0%', ease: "power2.out", duration: 0.5});
+
+//         await delay(1500);
+//         done();
+
+//       },
+//       enter(){
+
+//         gsap
+//         .to(wipe, {left: '100%', ease:"power2.in", duration: 0.5})
+//         .set(wipe, {left: '-100%'})
+
+//       }
+//     }
+//   ]
+
+// })
