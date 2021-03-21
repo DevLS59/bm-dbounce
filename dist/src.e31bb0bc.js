7a2cfee808ace65ca3d98bfba9238c138070c64f
devls@yahoo.com <devls@yahoo.com>
2021-03-21 20:26:48 +0100

    section songs

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/gsap/gsap-core.js":[function(require,module,exports) {
+},{"./..\\images\\cover.jpg":[["cover.9c094cd5.jpg","images/cover.jpg"],"images/cover.jpg"],"./..\\images\\cover-tarantino.jpg":[["cover-tarantino.ceff2ad2.jpg","images/cover-tarantino.jpg"],"images/cover-tarantino.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/gsap/gsap-core.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -7428,7 +7428,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53550" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52645" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
